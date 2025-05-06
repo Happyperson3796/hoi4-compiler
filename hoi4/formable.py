f1c435c8e5ac958926ef21c61850734649d1c8ce
BuildTools <unconfigured@null.spigotmc.org>
2025-05-06 08:23:15 -0400

    Push

--- hoi4/formable.py
+++ hoi4/formable.py
@@ -22,9 +22,11 @@ class Formable(fileType):
 
         id, data = self.define()
 
-        name = data.extract("name")
-        name_adj = data.extract("name_adj")
-        name_def = data.extract("name_def")
+        namespace = data.extract("namespace")
+
+        name = data.extract("name").replace("\"", "")
+        name_adj = data.extract("name_adj").replace("\"", "")
+        name_def = data.extract("name_def").replace("\"", "")
         name_extras = data.extract("name_extras", Collection())
         color = data.extract("color")
         allowed = data.extract("allowed")
@@ -36,16 +38,45 @@ class Formable(fileType):
         on_formed = data.extract("on_formed", "")
         extra_reqs = data.extract("extra_reqs", "")
 
-        #os.makedirs(parent_dir+"/localisation/", exist_ok=True)
-        #loc_file = parent_dir+"/localisation/"+namespace+"_dynamic_subideology_l_english.yml"
+        os.makedirs(parent_dir+"/localisation/", exist_ok=True)
+        loc_file = parent_dir+"/localisation/"+namespace+"_dynamic_formable_loc_"+"_l_english.yml"
 
-        #if not os.path.exists(loc_file):
-        #    with open(loc_file, "w", encoding="utf-8-sig") as file:
-        #        file.write("l_english:")
+        if not os.path.exists(loc_file):
+            with open(loc_file, "w", encoding="utf-8-sig") as file:
+                file.write("l_english:")
+
+        with open(loc_file, "a", encoding="utf-8-sig") as file:
+            template = f"""
+  formable_{id}_category:0 "Form {name_def}"
+  formable_{id}_category_desc:0 "Form {name_def}"
+  formable_view_{id}:0 "View Cores"
+  formable_form_{id}:0 "Form {name_def}"
+  formable_apply_{id}:0 "Re-Apply Cosmetic"
+  {id}_formable_cosmetic:0 "Akhand Bharat"
+  {id}_formable_cosmetic_ADJ:0 "Bharatyan"
+  {id}_formable_cosmetic_DEF:0 "{name_def}"
+  formable_{id}_core:0 "Integrate [FROM.GetName]"
+  formable_{id}_core_desc:0 "Cores [FROM.GetName] once over 50% compliance"
+
+
+  
+  formable_form_{id}_desc:0 "This is an exclusive formable! You will not be able to form any others after this.
+"""
+            file.write(template)
+
 
     def clean(self):
         head, tail = os.path.split(self.path)
         parent_dir = os.path.abspath(os.path.join(head, os.pardir))
+
+        id, data = self.define()
+
+        namespace = data.extract("namespace")
+
+        try:
+            os.remove(parent_dir+"/localisation/"+namespace+"_dynamic_formable_loc_"+"_l_english.yml")
+        except:
+            pass
 
 
     def required_dir(self):
@@ -65,6 +96,8 @@ class JsonFormable(Formable):
         for k in data.keys(): #Not case sensitive
             temp[k.lower()] = data[k]
         data = temp
+
+        data["color"] = data["color"].removeprefix("rgb(").removesuffix(")").strip().replace(",", " ").replace("  ", " ").split(" ")
 
         data = reformat(data)
 
