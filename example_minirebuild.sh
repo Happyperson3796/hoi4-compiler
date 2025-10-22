8ba6ea6346e77b809a3f1a3b8e2d9d4dfa78cf32
BuildTools <unconfigured@null.spigotmc.org>
2025-10-22 14:57:06 -0400

    Added demo mod, error message for antivirus permission error

--- example_minirebuild.sh
+++
@@ -1,5 +0,0 @@
-./build.sh
-source examples/.env/bin/activate
-pip install /home/happyperson3796/Downloads/hoi4-compiler/dist/hoi4-0.1-py3-none-any.whl --force-reinstall
-cd examples
-python z_minibuild.pyr
--- hoic.py
+++ hoic.py
@@ -12,6 +12,7 @@ if len(sys.argv) > 1:
         compiler.Build(file_to_open).build()
     except Exception as e:
         print(e)
+        print("If access is denied, try adding an exception to your antivirus for 'C:\Program Files (x86)\HoiCompiler\hoic.exe'")
     input("Press any key to exit . . .")
 else:
     input("No build file was specified!")
